55c32059863fbc38a1f7d78f94119badc4c655d0
Tsuyoshi Nagamine <t.nagamine1107@gmail.com>
2017-07-12 23:01:45 +0900

    初コミット

--- nlp100.py
+++ nlp100.py
@@ -2,54 +2,18 @@
 #
 # 00. 文字列の逆順
 # 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
-
-'''
-a = 'stressed'
-print (a)
-a = a[::-1] #スライス
-print (a)
-'''
-
 #
 # 01. 「パタトクカシーー」
 # 「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．
-
-'''
-a = 'パタトクカシーー'
-a_len = len(a)
-a = a[0:a_len:2]
-print (a)
-'''
-
 #
 # 02. 「パトカー」＋「タクシー」＝「パタトクカシーー」
 # 「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
-
-'''
-a = 'パトカー'
-b = 'タクシー'
-a_len = len(a)
-b_len = len(b)
-ab = []
-
-for count in range(a_len):
-    a_str = a[count]
-    b_str = b[count]
-    ab.append(a_str)
-    ab.append(b_str)
-
-ab ="".join(ab)
-print (ab)
-'''
-
 #
 # 03. 円周率
 # "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
-
 #
 # 04. 元素記号
 # "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."という文を単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭に2文字を取り出し，取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を作成せよ．
-
 #
 # 05. n-gram
 # 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
